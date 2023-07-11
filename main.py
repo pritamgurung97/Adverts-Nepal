dd37663457212f31c33872649432f09a7f342e74
pritamgurung97 <124842594+pritamgurung97@users.noreply.github.com>
2023-07-11 10:32:20 +0100

    Add register route and register form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
@@ -15,6 +15,8 @@ app = Flask(__name__)
 ckeditor = CKEditor(app)
 Bootstrap(app)
 app.config['SECRET_KEY'] = "hello"
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 #Database initilization
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -49,11 +51,27 @@ class Ad_details(FlaskForm):
     image_url = StringField('Image URL')
     submit = SubmitField('Post Ad')
 
+#Create a Flask_Form to register users.
+class Register_form(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    contact_number = IntegerField('Contact Number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register', validators=[DataRequired()])
+
 # Create the database
 # with app.app_context():
 #     db.create_all()
 
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
+# Get all the ads in the home page.
 def home():
     all_ads = Ad.query.all()
     return render_template('index.html', all_ads=all_ads)
@@ -70,9 +88,23 @@ def post_ad():
         db.session.commit()
     return render_template('post.html',form=form)
 
-
-
-
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    register_form = Register_form()
+    if register_form.validate_on_submit():
+        name = register_form.name.data
+        email = register_form.email.data
+        contact_number = register_form.contact_number.data
+        hashed_salted_password = generate_password_hash(register_form.password.data,method="pbkdf2:sha256",salt_length=8)
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('The email already exists, please try logging in instead')
+            return redirect(url_for('login'))
+        print(name)
+        new_user = User(name=name,email=email,contact_number=contact_number,password=hashed_salted_password)
+        db.session.add(new_user)
+        db.session.commit()
+    return render_template('register.html', register_form=register_form)
 
 
 
